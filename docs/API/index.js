7bdd28ea8727c2bdbfab9435faf968ed2efbbf42
Jathin Sreenivas <sjathin@gmail.com>
2020-12-31 01:06:52 +0100

    Swagger Editor for routes documentation

---
+++ docs/API/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const swaggerUI = require('swagger-ui-express');
+const YAML = require('yamljs');
+const swaggerDocument = YAML.load('./TeamLithium.yaml');
+//const swaggerDocument = require('./swagger.json'//////////////////
+
+app.use('/', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
+
+console.log('Starting API documnetation on localhost 1002')
+
+app.listen(1002);
