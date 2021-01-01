df71564f7daefe7726816d5a529f29bf78c1d2e6
Jathin Sreenivas <sjathin@gmail.com>
2021-01-01 11:12:58 +0100

    Code indentation fixes using ESlint

--- app/patient-asset-transfer/application-javascript/app.js
+++ app/patient-asset-transfer/application-javascript/app.js
@@ -2,17 +2,16 @@
  * @author Jathin Sreenivas
  * @email jathin.sreenivas@stud.fra-uas.de
  * @create date 2020-12-26 13:26:42
- * @modify date 2020-12-29 23:28:32
+ * @modify date 2021-01-01 11:09:40
  * @desc The file which interacts with the fabric network.
  */
 
-'use strict';
 
-const { Gateway, Wallets } = require('fabric-network');
+const {Gateway, Wallets} = require('fabric-network');
 const FabricCAServices = require('fabric-ca-client');
 const path = require('path');
-const { buildCAClient, registerAndEnrollUser } = require('./CAUtil.js');
-const { buildCCPHosp2, buildCCPHosp1, buildWallet } = require('./AppUtil.js');
+const {buildCAClient, registerAndEnrollUser} = require('./CAUtil.js');
+const {buildCCPHosp2, buildCCPHosp1, buildWallet} = require('./AppUtil.js');
 
 const channelName = 'hospitalchannel';
 const chaincodeName = 'patient';
@@ -21,204 +20,158 @@ const mspOrg2 = 'hosp2MSP';
 const walletPath = path.join(__dirname, 'wallet');
 
 
-const util = require('util');
-
 /**
  * @author Jathin Sreenivas
- * @param  {string} inputString
- * @returns {string} jsonString
- * @description Formats the string to JSON 
- */
-function prettyJSONString(inputString) {
-	return JSON.stringify(JSON.parse(inputString), null, 2);
-}
-
-
-/**
- * @author Jathin Sreenivas
- * @param  {string} userName
- * @returns {networkObj} networkObj if all paramters are correct, the networkObj consists of contract, network, gateway
- * @returns {string} response error if there is a error in the method
- * @description Connects to the network using the username - doctorID, networkObj contains the paramters using which 
+ * @param  {string} doctorID
+ * @return {networkObj} networkObj if all paramters are correct, the networkObj consists of contract, network, gateway
+ * @return {string} response error if there is a error in the method
+ * @description Connects to the network using the username - doctorID, networkObj contains the paramters using which
  * @description a connection to the fabric network is possible.
  */
-exports.connectToNetwork = async function (doctorID) {
-  
-	const gateway = new Gateway();
-	const ccp = buildCCPHosp1();
-  
-	try {
-		
-		const walletPath = path.join(process.cwd(), '../patient-asset-transfer/application-javascript/wallet/');
+exports.connectToNetwork = async function(doctorID) {
+  const gateway = new Gateway();
+  const ccp = buildCCPHosp1();
 
-		const wallet = await buildWallet(Wallets, walletPath);
+  try {
+    const walletPath = path.join(process.cwd(), '../patient-asset-transfer/application-javascript/wallet/');
 
-		const userExists = await wallet.get(doctorID);
-        if (!userExists) {
-            console.log('An identity for the doctorID: ' + doctorID + ' does not exist in the wallet');
-			console.log('Create the doctorID before retrying');
-			let response = {};
-			response.error = 'An identity for the user ' + doctorID + ' does not exist in the wallet. Register ' + doctorID + ' first';
-			return response;
-        }
-	
+    const wallet = await buildWallet(Wallets, walletPath);
 
-		/** 
-		* setup the gateway instance
-		*  he user will now be able to create connections to the fabric network and be able to
-		* ubmit transactions and query. All transactions submitted by this gateway will be
-		* signed by this user using the credentials stored in the wallet.
-		*/
-		await gateway.connect(ccp, {
-			wallet,
-			identity: doctorID,
-			discovery: { enabled: true, asLocalhost: true } // using asLocalhost as this gateway is using a fabric network deployed locally
-		});
-	
-		// Build a network instance based on the channel where the smart contract is deployed
-		const network = await gateway.getNetwork(channelName);
+    const userExists = await wallet.get(doctorID);
+    if (!userExists) {
+      console.log('An identity for the doctorID: ' + doctorID + ' does not exist in the wallet');
+      console.log('Create the doctorID before retrying');
+      const response = {};
+      response.error = 'An identity for the user ' + doctorID + ' does not exist in the wallet. Register ' + doctorID + ' first';
+      return response;
+    }
 
-		// Get the contract from the network.
-		const contract = network.getContract(chaincodeName);	
-	
-		let networkObj = {
-			contract: contract,
-			network: network,
-			gateway: gateway
-		};
-	
-		return networkObj;
-	
-	} catch (error) {
-		console.log(`Error processing transaction. ${error}`);
-		console.log(error.stack);
-		let response = {};
-		response.error = error;
-		return response;
-	} finally {
-		console.log('Succesfully connected to the network.');
-		// gateway.disconnect();
-	}
+    /**
+    * setup the gateway instance
+    *  he user will now be able to create connections to the fabric network and be able to
+    * ubmit transactions and query. All transactions submitted by this gateway will be
+    * signed by this user using the credentials stored in the wallet.
+    */
+    // using asLocalhost as this gateway is using a fabric network deployed locally
+    await gateway.connect(ccp, {wallet, identity: doctorID, discovery: {enabled: true, asLocalhost: true}});
+
+    // Build a network instance based on the channel where the smart contract is deployed
+    const network = await gateway.getNetwork(channelName);
+
+    // Get the contract from the network.
+    const contract = network.getContract(chaincodeName);
+
+    const networkObj = {
+      contract: contract,
+      network: network,
+      gateway: gateway,
+    };
+
+    return networkObj;
+  } catch (error) {
+    console.log(`Error processing transaction. ${error}`);
+    console.log(error.stack);
+    const response = {};
+    response.error = error;
+    return response;
+  } finally {
+    console.log('Succesfully connected to the network.');
+    // gateway.disconnect();
+  }
 };
 
 
 /**
  * @author Jathin Sreenivas
  * @param  {*} networkObj the object which is given when connectToNetwork is executed
- * @param  {boolean} isQuery true if retieving from ledger, else false in the case of add a transaction to the ledger. 
+ * @param  {boolean} isQuery true if retieving from ledger, else false in the case of add a transaction to the ledger.
  * @param  {string} func must be the function name in the chaincode.
- * @param  {string} args - a json string, if there are mutiple args, the args must be a json as one string 
- * @returns {string} response if the transaction was successful
- * @returns {string} response error otherwise
+ * @param  {string} args - a json string, if there are mutiple args, the args must be a json as one string
+ * @return {string} response if the transaction was successful
+ * @return {string} response error otherwise
  * @description A common function to interact with the ledger
  */
-exports.invoke = async function (networkObj, isQuery, func, args) {
-	
-	try {
-	
-		if (isQuery === true) {
-			
-			if (args) {
-
-				let response = await networkObj.contract.evaluateTransaction(func, args);
-				await networkObj.gateway.disconnect();
-				return response;
-			
-			} else {
-	
-				let response = await networkObj.contract.evaluateTransaction(func);
-				await networkObj.gateway.disconnect();
-				return response;
-			}
-		} else {
-			if (args) {
-		
-				args = JSON.parse(args[0]);
-				args = JSON.stringify(args);
-
-				let response = await networkObj.contract.submitTransaction(func, args);
-			
-				await networkObj.gateway.disconnect();
-			
-				return response;
-
-			} else {
-				let response = await networkObj.contract.submitTransaction(func);
-
-				await networkObj.gateway.disconnect();
-			
-				return response;
-			}
-		}
-  
-	} catch (error) {
-		console.error(`Failed to submit transaction: ${error}`);
-		return error;
-	}
+exports.invoke = async function(networkObj, isQuery, func, args) {
+  try {
+    if (isQuery === true) {
+      if (args) {
+        const response = await networkObj.contract.evaluateTransaction(func, args);
+        await networkObj.gateway.disconnect();
+        return response;
+      } else {
+        const response = await networkObj.contract.evaluateTransaction(func);
+        await networkObj.gateway.disconnect();
+        return response;
+      }
+    } else {
+      if (args) {
+        args = JSON.parse(args[0]);
+        args = JSON.stringify(args);
+        const response = await networkObj.contract.submitTransaction(func, args);
+        await networkObj.gateway.disconnect();
+        return response;
+      } else {
+        const response = await networkObj.contract.submitTransaction(func);
+        await networkObj.gateway.disconnect();
+        return response;
+      }
+    }
+  } catch (error) {
+    console.error(`Failed to submit transaction: ${error}`);
+    return error;
+  }
 };
 
 /**
  * @author Jathin Sreenivas
  * @param  {string} hospitalId representing the hospital to which the user/doctor has to be created
- * @param  {string} doctorId 
+ * @param  {string} doctorId
  * @description Creates a user/doctor and adds to the wallet to the given hospital
  */
-exports.registerDoctor = async function (hospitalId ,doctorId) {
+exports.registerDoctor = async function(hospitalId, doctorId) {
+  if (!doctorId || !hospitalId) {
+    const response = {};
+    response.error = 'Error! You need to fill all fields before you can register!';
+    return response;
+  }
 
-  
-	if (!doctorId || !hospitalId) {
-	  let response = {};
-	  response.error = 'Error! You need to fill all fields before you can register!';
-	  return response;
-	}
-  
-	try {
-		
-		const wallet = await buildWallet(Wallets, walletPath);
-
-		if (hospitalId == 1) {
-			const ccp = buildCCPHosp1();
-			const caClient = buildCAClient(FabricCAServices, ccp, 'ca.hosp1.lithium.com');
-			await registerAndEnrollUser(caClient, wallet, mspOrg1, doctorId, 'admin');
-		} else {
-			const ccp = buildCCPHosp2();
-			const caClient = buildCAClient(FabricCAServices, ccp, 'ca.hosp2.lithium.com');
-			await registerAndEnrollUser(caClient, wallet, mspOrg2, doctorId, 'admin');
-		}
-		
-		console.log(`Successfully registered doctor . Use doctorId ${doctorId} to login above.`);
-		let response = `Successfully registered doctor . Use doctorId ${doctorId} to login above.`;
-		return response;
-
-	} catch (error) {
-		console.error(`Failed to register user + ${doctorId} + : ${error}`);
-		let response = {};
-		response.error = error;
-		return response;
-	}
+  try {
+    const wallet = await buildWallet(Wallets, walletPath);
+    if (hospitalId === 1) {
+      const ccp = buildCCPHosp1();
+      const caClient = buildCAClient(FabricCAServices, ccp, 'ca.hosp1.lithium.com');
+      await registerAndEnrollUser(caClient, wallet, mspOrg1, doctorId, 'admin');
+    } else {
+      const ccp = buildCCPHosp2();
+      const caClient = buildCAClient(FabricCAServices, ccp, 'ca.hosp2.lithium.com');
+      await registerAndEnrollUser(caClient, wallet, mspOrg2, doctorId, 'admin');
+    }
+    console.log(`Successfully registered doctor . Use doctorId ${doctorId} to login above.`);
+    const response = `Successfully registered doctor . Use doctorId ${doctorId} to login above.`;
+    return response;
+  } catch (error) {
+    console.error(`Failed to register user + ${doctorId} + : ${error}`);
+    const response = {};
+    response.error = error;
+    return response;
+  }
 };
 
 
 /* TEMP */
 
-exports.registerPatient = async function (networkObj ,patientId, firstName, lastName, age, address) {
-
-		try {
-	
-			console.log('\n--> Submit Transaction: CreateAsset, creates new asset with ID, color, owner, size, and appraisedValue arguments');
-			let result = await networkObj.contract.submitTransaction('createPatient', patientId, firstName, lastName, age, address);
-
-			console.log(`Successfully registered Patient . Use patientId ${patientId} to login above.`);
-			let response = `Successfully registered Patient . Use patientId ${patientId} to login above.`;
-			return response;
-
-		} catch (error) {
-			console.error(`Failed to register patient + ${patientId} + : ${error}`);
-			let response = {};
-			response.error = error;
-			return response;
-		}
-
+exports.registerPatient = async function(networkObj, patientId, firstName, lastName, age, address) {
+  try {
+    console.log('\n--> Submit Transaction: CreateAsset, creates new asset with ID, color, owner, size, and appraisedValue arguments');
+    console.log(`Successfully registered Patient . Use patientId ${patientId} to login above.`);
+    const response = `Successfully registered Patient . Use patientId ${patientId} to login above.`;
+    return response;
+  } catch (error) {
+    console.error(`Failed to register patient + ${patientId} + : ${error}`);
+    const response = {};
+    response.error = error;
+    return response;
+  }
 };
 
 
--- app/server/enrollAdmin-Hospital1.js
+++ app/server/enrollAdmin-Hospital1.js
@@ -2,48 +2,50 @@
  * @author Jathin Sreenivas
  * @email jathin.sreenivas@stud.fra-uas.de
  * @create date 2020-12-26 13:26:42
- * @modify date 2020-12-29 20:55:25
+ * @modify date 2020-12-31 14:44:13
  * @desc Execute this file to create and enroll an admin at Hospital 1.
  */
 
-'use strict';
 
-const { Wallets } = require('fabric-network');
+const {Wallets} = require('fabric-network');
 const FabricCAServices = require('fabric-ca-client');
 const path = require('path');
-const { buildCAClient, enrollAdmin } = require('../patient-asset-transfer/application-javascript/CAUtil.js');
-const { buildCCPHosp1, buildWallet } = require('../patient-asset-transfer/application-javascript/AppUtil.js');
+const {buildCAClient, enrollAdmin} = require('../patient-asset-transfer/application-javascript/CAUtil.js');
+const {buildCCPHosp1, buildWallet} = require('../patient-asset-transfer/application-javascript/AppUtil.js');
 const adminHospital1 = 'admin';
 const adminHospital1Passwd = 'adminpw';
 
 const mspHosp1 = 'hosp1MSP';
 const walletPath = path.join(__dirname, '../patient-asset-transfer/application-javascript/wallet');
 
+// Temporary DB
+const {addUser} = require('./Hosp1LocalDB.js');
 
-
+/**
+ *
+ */
 async function main() {
-    try {
-		
-        // build an in memory object with the network configuration (also known as a connection profile)
-		const ccp = buildCCPHosp1();
+  try {
+    // build an in memory object with the network configuration (also known as a connection profile)
+    const ccp = buildCCPHosp1();
 
-		// build an instance of the fabric ca services client based on
-		// the information in the network configuration
-		const caClient = buildCAClient(FabricCAServices, ccp, 'ca.hosp1.lithium.com');
+    // build an instance of the fabric ca services client based on
+    // the information in the network configuration
+    const caClient = buildCAClient(FabricCAServices, ccp, 'ca.hosp1.lithium.com');
 
-		// setup the wallet to hold the credentials of the application user
-        const wallet = await buildWallet(Wallets, walletPath);
+    // setup the wallet to hold the credentials of the application user
+    const wallet = await buildWallet(Wallets, walletPath);
 
-		// to be executed and only once per hospital. Which enrolls admin and creates admin in the wallet
-        await enrollAdmin(caClient, wallet, mspHosp1, adminHospital1, adminHospital1Passwd);
-        
-        console.log('msg: Successfully enrolled admin user ' + adminHospital1 + ' and imported it into the wallet');
+    // to be executed and only once per hospital. Which enrolls admin and creates admin in the wallet
+    await enrollAdmin(caClient, wallet, mspHosp1, adminHospital1, adminHospital1Passwd);
 
-    }catch (error) {
-		
-		console.error(`Failed to enroll admin user ' + ${adminHospital1} + : ${error}`);
-        process.exit(1);
-	}
+    console.log('msg: Successfully enrolled admin user ' + adminHospital1 + ' and imported it into the wallet');
+
+    addUser(adminHospital1, adminHospital1Passwd, 'admin');
+  } catch (error) {
+    console.error(`Failed to enroll admin user ' + ${adminHospital1} + : ${error}`);
+    process.exit(1);
+  }
 }
 
 main();
