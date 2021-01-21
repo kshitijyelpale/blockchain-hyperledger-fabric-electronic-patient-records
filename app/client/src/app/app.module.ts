11002689feabebcc22bc3de84785ea567ce9a64f
Kshitij <kshitij.yelpale@gmail.com>
2021-01-21 01:35:42 +0100

    added some basic components and nav html

--- app/client/src/app/app.module.ts
+++ app/client/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { AdminComponent } from './admin/admin.component';
+import { PatientComponent } from './patient/patient.component';
+import { DoctorComponent } from './doctor/doctor.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    AdminComponent,
+    PatientComponent,
+    DoctorComponent
   ],
   imports: [
     BrowserModule,
---
+++ app/client/src/app/doctor/doctor.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-doctor',
+  templateUrl: './doctor.component.html',
+  styleUrls: ['./doctor.component.scss']
+})
+export class DoctorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
