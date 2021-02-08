dd2574d4f0b640e638c30d8427ca98e9331606d6
Jathin Sreenivas <sjathin@gmail.com>
2021-02-08 13:01:40 +0100

    Register User dynamic based on hospitalID

--- app/server/registerUser.js
+++ app/server/registerUser.js
@@ -2,7 +2,7 @@
  * @author Jathin Sreenivas
  * @email jathin.sreenivas@stud.fra-uas.de
  * @create date 2020-12-26 13:26:42
- * @modify date 2021-01-30 10:54:04
+ * @modify date 2021-02-08 12:44:25
  * @desc This file creates a user named 'appUser' at Hospital 1. (Just for testing. Use the API to create a patient)
  */
 
@@ -13,31 +13,46 @@ const path = require('path');
 const {buildCAClient, registerAndEnrollUser} = require('../patient-asset-transfer/application-javascript/CAUtil.js');
 const walletPath = path.join(__dirname, '/../patient-asset-transfer/application-javascript/wallet');
 const {buildCCPHosp1, buildWallet} = require('../patient-asset-transfer/application-javascript/AppUtil.js');
-const mspOrg1 = 'hosp1MSP';
-const adminHospital1 = 'hosp1admin';
+let mspOrg;
+let adminUserId;
+let caClient;
 
 /**
- * @param {string} hosp1UserId
+ * @param {String} hospitalId
+ * @param {string} userId
  * @param {String} attributes
  */
-exports.enrollRegisterUser = async function(hosp1UserId, attributes) {
+exports.enrollRegisterUser = async function(hospitalId, userId, attributes) {
   try {
-    // build an in memory object with the network configuration (also known as a connection profile)
-    const ccp = buildCCPHosp1();
-
-    // build an instance of the fabric ca services client based on
-    // the information in the network configuration
-    const caClient = buildCAClient(FabricCAServices, ccp, 'ca.hosp1.lithium.com');
-
     // setup the wallet to hold the credentials of the application user
     const wallet = await buildWallet(Wallets, walletPath);
+    hospitalId = parseInt(hospitalId);
+    if (hospitalId === 1) {
+      // build an in memory object with the network configuration (also known as a connection profile)
+      const ccp = buildCCPHosp1();
 
-    // enrolls users to Hospital 1 and adds the user to the wallter
-    await registerAndEnrollUser(caClient, wallet, mspOrg1, hosp1UserId, adminHospital1, attributes);
+      // build an instance of the fabric ca services client based on
+      // the information in the network configuration
+      caClient = buildCAClient(FabricCAServices, ccp, 'ca.hosp1.lithium.com');
 
-    console.log('msg: Successfully enrolled user ' + hosp1UserId + ' and imported it into the wallet');
+      mspOrg = 'hosp1MSP';
+      adminUserId = 'hosp1admin';
+    } else {
+      // build an in memory object with the network configuration (also known as a connection profile)
+      const ccp = buildCCPHosp2();
+
+      // build an instance of the fabric ca services client based on
+      // the information in the network configuration
+      caClient = buildCAClient(FabricCAServices, ccp, 'ca.hosp2.lithium.com');
+
+      mspOrg = 'hosp2MSP';
+      adminUserId = 'hosp2admin';
+    }
+    // enrolls users to Hospital 1 and adds the user to the wallet
+    await registerAndEnrollUser(caClient, wallet, mspOrg, userId, adminUserId, attributes);
+    console.log('msg: Successfully enrolled user ' + userId + ' and imported it into the wallet');
   } catch (error) {
-    console.error(`Failed to register user "${hosp1UserId}": ${error}`);
+    console.error(`Failed to register user "${userId}": ${error}`);
     process.exit(1);
   }
 };
