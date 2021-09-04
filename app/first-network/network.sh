838ffb05246e738835117927a61841b9520e2136
Jathin Sreenivas <sjathin@gmail.com>
2021-09-04 15:45:08 +0530

    Bug fix - Docker does not consider env file, error during creating channel

--- app/first-network/network.sh
+++ app/first-network/network.sh
@@ -181,7 +181,7 @@ function createOrgs() {
 
     infoln "Generate certificates using Fabric CA's"
 
-    IMAGE_TAG=${CA_IMAGETAG} docker-compose -f $COMPOSE_FILE_CA up -d 2>&1
+    IMAGE_TAG=${CA_IMAGETAG} docker-compose --env-file ./.env -f $COMPOSE_FILE_CA up -d 2>&1 
 
     . organizations/fabric-ca/registerEnroll.sh
 
@@ -281,9 +281,9 @@ function networkUp() {
     COMPOSE_FILES="${COMPOSE_FILES} -f ${COMPOSE_FILE_COUCH}"
   fi
 
-  IMAGE_TAG=$IMAGETAG docker-compose ${COMPOSE_FILES} up -d 2>&1
+  IMAGE_TAG=$IMAGETAG docker-compose --env-file ./.env ${COMPOSE_FILES} up -d 2>&1
 
-  docker-compose -f "${COMPOSE_FILE_REDIS}" up -d
+  docker-compose --env-file ./.env -f "${COMPOSE_FILE_REDIS}" up -d
 
   docker ps -a
   if [ $? -ne 0 ]; then
@@ -340,8 +340,8 @@ function deployCC3() {
 # Tear down running network
 function networkDown() {
   # stop org3 containers also in addition to org1 and org2, in case we were running sample to add org3
-  docker-compose -f $COMPOSE_FILE_BASE -f $COMPOSE_FILE_COUCH -f $COMPOSE_FILE_CA -f $COMPOSE_FILE_REDIS down --volumes --remove-orphans
-  docker-compose -f $COMPOSE_FILE_COUCH_ORG3 -f $COMPOSE_FILE_ORG3 down --volumes --remove-orphans
+  docker-compose --env-file ./.env -f $COMPOSE_FILE_BASE -f $COMPOSE_FILE_COUCH -f $COMPOSE_FILE_CA -f $COMPOSE_FILE_REDIS down --volumes --remove-orphans
+  docker-compose --env-file ./.env -f $COMPOSE_FILE_COUCH_ORG3 -f $COMPOSE_FILE_ORG3 down --volumes --remove-orphans
   # Don't remove the generated artifacts -- note, the ledgers are always removed
   if [ "$MODE" != "restart" ]; then
     # Bring down the network, deleting the volumes
